996c58e8e9b5e0fe9b1f88fe88be4f63278773b7
osamaHamad0 <>
2021-04-25 22:36:23 +0000

    fix javascript function error

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // function to go to linus' github
-function githubButton() {
-  window.location.href = 'https://github.com/torvalds';
+function githubButton () {
+  window.location.href = 'https://github.com/torvalds'
 };
 
 // function to go to linus' wiki
-function wikiButton() {
-  window.location.href = 'https://en.wikipedia.org/wiki/Linus_Torvalds';
+function wikiButton () {
+  window.location.href = 'https://en.wikipedia.org/wiki/Linus_Torvalds'
 };
 
 // function to go to linus' twitter
-function twitterButton() {
-  window.location.href = 'https://twitter.com/linus__torvalds?lang=en';
+function twitterButton () {
+  window.location.href = 'https://twitter.com/linus__torvalds?lang=en'
 };
