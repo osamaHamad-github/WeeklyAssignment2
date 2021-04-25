b52d2ab77eef3b1163d1116838d6ade00edf0679
osamaHamad0 <>
2021-04-25 22:44:28 +0000

    fix javascript functions

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // function to go to linus' github
 function githubButton () {
   window.location.href = 'https://github.com/torvalds'
+  return window.loaction.href
 };
 
 // function to go to linus' wiki
 function wikiButton () {
   window.location.href = 'https://en.wikipedia.org/wiki/Linus_Torvalds'
+  return window.loaction.href
 };
 
 // function to go to linus' twitter
 function twitterButton () {
   window.location.href = 'https://twitter.com/linus__torvalds?lang=en'
+  return window.loaction.href
 };
